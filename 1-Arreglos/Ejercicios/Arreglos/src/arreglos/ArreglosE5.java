c7e6b9968fbc85179f674d0b9ec4bca93dbff7d2
denisdimarco <denisdimarcoleda@gmail.com>
2021-09-01 18:12:28 -0300

    ejercicio 5

---
+++ 1-Arreglos/Ejercicios/Arreglos/src/arreglos/ArreglosE5.java
@@ -0,0 +1,24 @@
+/*
+Vectores - Ejercicio 5:
+
+En tres vectores diferentes se guardan los nombres, temperaturas mínimas y máximas de
+5 ciudades de la provincia de Misiones.
+ 
+En el primer vector se guardan los nombres de las ciudades, 
+en el segundo las temperaturas mínimas alcanzadas y 
+en el tercero lastemperaturas máximas alcanzadas en la última semana. 
+
+Se necesita un programa que permita la carga de las ciudades, sus temperaturas mínimas y máximas; 
+además, deberá poder informar por pantalla cual fue la ciudad con la temperatura más baja y cual 
+con la temperatura más alta (dando a conocer al mismo tiempo la cantidad de grados).
+ */
+
+package arreglos;
+
+/**
+ *
+ * @author Denis
+ */
+public class ArreglosE5 {
+    
+}
