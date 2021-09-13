e56bec2528a23fab5f9d78132262b63c6203eb8d
denisdimarco <denisdimarcoleda@gmail.com>
2021-09-13 08:45:55 -0300

    Update logica.java

--- InterfazGrafica/diMarco_Denis_tpo1/src/logica/logica.java
+++ InterfazGrafica/diMarco_Denis_tpo1/src/logica/logica.java
@@ -3,16 +3,13 @@ package logica;
 import igu.Principal;
 
 public class logica {
-    
+
     public static void main(String[] args) {
 
-        
-        
         Principal pantalla = new Principal();
         pantalla.setVisible(true);
         pantalla.setLocationRelativeTo(null);
-        
-        
+
     }
-    
+
 }
